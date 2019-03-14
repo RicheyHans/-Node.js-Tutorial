1e08ebe210d9c208060ac645986bb956cb75a302
RicheyHans <ifyoutolerate@gmail.com>
2019-03-15 00:53:39 +0900

    add handler has been finished

--- Resources/JsonRpcExample/handlers/handler_info.js
+++ Resources/JsonRpcExample/handlers/handler_info.js
@@ -1,7 +1,9 @@
 console.log('handler_info 파일 로딩됨');
 
 var handler_info = [
-    {file: './echo', method: 'echo'}
+    {file: './echo', method: 'echo'},
+    {file: './echo_error', method: 'echo_error'},
+    {file: './add', method: 'add'}
 ];
 
 module.exports = handler_info;
